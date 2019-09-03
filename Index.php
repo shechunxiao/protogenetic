44555863e8872dd46e1f45b469e29ea2d0d6d73b
scx <282074579@qq.com>
2019-09-03 17:51:56 +0800

    回调测试学习

--- Index.php
+++ Index.php
@@ -3,6 +3,7 @@
 use app\Controller\FirstController;
 use app\Controller\Man;
 use app\Controller\SecondController;
+use app\Controller\ThisTest;
 use app\Controller\ThreeImplements;
 
 require 'bootstrap/app.php';
@@ -18,4 +19,22 @@ echo '<br>';
 $c = new Man(new ThreeImplements());
 $c->groUp();
 echo '<br>';
+
+$thisTest = new ThisTest();
+$thi = $thisTest->index();
+$get_class = get_class($thisTest);
+print_r($get_class);
+$thisTest->check();
+echo '<br/>';
+
+$d = 3;
+$res = $thisTest->close(1, function ($thisTest) {
+    return $thisTest->test();
+});
+print_r($res);
+
+
+
+
+
 
---
+++ app/Controller/ThisTest.php
@@ -0,0 +1,29 @@
+<?php
+namespace app\Controller;
+use Closure;
+class ThisTest{
+    protected $m;
+    public function __construct()
+    {
+        $this->m = 222;
+    }
+    public function index(){
+       return $this;
+    }
+    public function check(){
+        if (method_exists(static::class,'index')){
+            echo 'index方法存在';
+        }else{
+            echo 'index方法不存在';
+        }
+    }
+    public function close($a,Closure $callback){
+//        $result = call_user_func($callback,10,20);
+        $result = $callback($this);
+        return $result.$a;
+    }
+    public function test(){
+        echo 222;
+    }
+
+}
